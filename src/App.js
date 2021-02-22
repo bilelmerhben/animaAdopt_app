dbc8d0d222fe88de7fbed42c98e674e509d0002d
bilelmerhben <53703326+bilelmerhben@users.noreply.github.com>
2021-02-22 19:32:19 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./Navbar";
 
-
-
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
