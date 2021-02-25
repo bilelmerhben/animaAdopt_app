546be6eaf8c6f050e2f40f79b5bc52dd01211b98
bilelmerhben <53703326+bilelmerhben@users.noreply.github.com>
2021-02-25 13:25:14 +0100

    card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Navbar from "./Navbar";
-
 import React from 'react';
+import Navbar from "./Navbar";
+import Card from "./Card";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Card/>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,35 +2,58 @@
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="/">AnimaAdopt</a>
+          
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                  <a class="nav-link" href="/">Acceuil</a>
+                </li>
+                <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="/services" id="navbarDropdown" 
+          role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/service1">Service1</a></li>
+            <li><a class="dropdown-item" href="/service2">Service2</a></li>
+            <li><a class="dropdown-item" href="/service3">Service3</a></li>
+          </ul>
+               </li>
+               <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="/associations" id="navbarDropdown" 
+          role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             Associations
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/association1">Association1</a></li>
+            <li><a class="dropdown-item" href="/association2">Association2</a></li>
+            <li><a class="dropdown-item" href="/association3">Association3</a></li>
+          </ul>
+               </li>
+
+            
+                <li class="nav-item">
+                  <a class="nav-link" href="/inscription" >S'inscrire</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/">English</a>
+                </li>
+              </ul>
+              {/*<form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+              */}
+              </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
 }
